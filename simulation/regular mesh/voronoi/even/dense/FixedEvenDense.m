928b231bdd3e8791fab03541eb40e6f81abe0d7a
JDIJason <jason9263@163.com>
2018-06-04 23:02:58 -0400

    sync save 64 result.

--- simulation/regular mesh/voronoi/even/dense/FixedEvenDense.m
+++ simulation/regular mesh/voronoi/even/dense/FixedEvenDense.m
@@ -20,11 +20,21 @@ title('Data Fraction vs \sigma');
 legend('With Front-end');
 hold off
 
-% figure
-% hold on
-% tline = [7 14 15 10 6 3 1];
-% nunitm(tline);
-% nunitm_no(tline);
-% legend('With Front-end','Wihtout Front-end');
 %%
+addpath(genpath('D:\Dropbox\Prelim_Defense\simulation\lib'));
+figure
+hold on
+tline = [7 14 15 10 6 3 1];
+nunitm(tline);
+grid on
+legend('With Front-end');
+title('Speedup vs \sigma');
+hold off
 
+figure
+hold on
+fraction(tline);
+grid on
+title('Data Fraction vs \sigma');
+legend('With Front-end');
+hold off
