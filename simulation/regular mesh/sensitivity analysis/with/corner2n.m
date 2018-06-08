2374cf24ef19794b837d3bdabdc6c74a5a13739c
JDIJason <jason9263@163.com>
2018-06-07 21:53:21 -0400

    sync

---
+++ simulation/regular mesh/sensitivity analysis/with/corner2n.m
@@ -0,0 +1,86 @@
+
+%%
+%sensitivity analysis of the regular mesh
+clear
+clc
+addpath(genpath('D:\Dropbox\Prelim_Defense\simulation\lib'));
+X = [];
+X = [2;X];
+
+len = 20;
+Y = cell(1,len+1);
+Y{1} = [1 1];
+
+for i = 1:len
+    tmp = 2 + 2*i;
+    X = [X;tmp];
+    
+    tmpj = [];
+    
+    for j = 1:i
+        tmpj =[tmpj,2];
+    end
+    
+    tmpj = [1,tmpj];
+    tmpj = [tmpj,1];
+    
+    Y{i+1} = tmpj;
+    
+end
+
+%%
+minsig = 0.01;
+maxsig = 0.99;
+
+[x,y] = meshgrid(0.01:0.01:0.99, 2:2:20);
+
+z = zeros(size(x));
+sizex = size(x);
+
+for i = 1: sizex(1)
+    for j = 1:sizex(2)
+        
+        B = sigmaB(Y{y(i,j)/2});
+        A = sigmaA(x(i,j),Y{y(i,j)/2});
+        times = A\B;
+        
+        z(i,j)  = 1/( times(1));
+    end
+end
+figure
+hold on
+surf(x,y,z,'FaceAlpha',0.7);
+xlabel('\sigma');
+ylabel('Number of cores');
+zlabel('Speedup');
+title('Speedup vs \sigma, Number of cores');
+colorbar
+
+hold off
+%%
+figure
+hold on
+x = [0.01,0.1,0.3,0.6,0.99];
+y = 2:2:20;
+sx = size(x);
+for i = 1:sx(2)
+    z = [];
+    for j = 2:2:20
+        tmp = j/2;
+        B = sigmaB(Y{tmp});
+        A = sigmaA(x(i),Y{tmp});
+        times = A\B;
+        tmpz  = 1/( times(1));
+        z = [z;tmpz];
+    end
+    plot(y,z);
+end
+
+grid on
+xlabel('Number of cores');
+ylabel('Speedup');
+title('Speedup vs Number of cores');
+legend('\sigma = 0.01','\sigma = 0.1','\sigma = 0.3','\sigma = 0.6','\sigma = 0.99');
+ylim([0,20]);
+hold off
+
---
+++ simulation/torus/power equal/with/vtorus4.m
@@ -0,0 +1,90 @@
+%%voronoi torus
+%
+clear
+clc
+%init the grid and seed info
+size = 50*3;
+%setup the location of seed
+numseed = 10*9;
+
+[pos,mcolor] = initvoronoi;
+
+neighbor = zeros(numseed,2*size);
+maxv = zeros(numseed,1);
+
+R = 5;
+r = 1;
+
+%%
+%manhattan distance
+figure
+hold on
+
+for i = 50:100
+    for j = 50:100
+        dis = [];
+        
+        for k = 1:numseed
+            tmp = abs(i - pos(k,1)) + abs(j - pos(k,2));
+            dis = [dis, tmp];
+        end
+        
+        [minv,index] = min(dis);
+        
+        if minv > 0
+            neighbor(index,minv) = neighbor(index,minv) + 1;
+            maxv(index) = max(maxv(index),minv);
+        end
+        
+        u = (i-50)/50;
+        v = (j-50)/50;
+        
+        u = u*2*pi;
+        v = v*2*pi;
+        
+        x=(R+r*cos(v))*cos(u);
+        y=(R+r*cos(v))*sin(u);
+        z=r*sin(v);
+        
+        
+        if minv <= 15
+            if mod(index,10) == 0
+                plot3(x,y,z,'o','MarkerEdgeColor',[mcolor(10,:)],'MarkerFaceColor',[mcolor(10,:)]);
+            else
+                plot3(x,y,z,'o','MarkerEdgeColor',[mcolor(mod(index,10),:)],'MarkerFaceColor',[mcolor(mod(index,10),:)]);
+            end
+            
+        end
+        
+    end
+end
+grid on
+
+%%
+u = [];
+v = [];
+
+for i = 50:100
+    for j = 50:100
+        tu = (i-50)/50;
+        tu = tu * 2* pi;
+        
+        u = [u tu];
+        tv = (j-50)/50;
+        tv = tv*2*pi;
+        
+        v =[v tv];
+    end
+end
+
+[u,v]=meshgrid(u,v);
+
+x=(R+r.*cos(v)).*cos(u);
+y=(R+r.*cos(v)).*sin(u);
+z=r.*sin(v);
+mesh(x,y,z);
+view([-52,64]);
+
+xlabel('longitude torus geographical index');
+ylabel('latitudinal torus geographical index');
+title('Placement of Processors');
