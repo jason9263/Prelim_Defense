481035bc08ae097d1319f7922fa3077ce29197c8
JDIJason <jason9263@163.com>
2018-06-29 13:10:19 -0400

    sync

--- simulation/torus/voronoi/dense and sparse/with/tVoronoisd.m
+++ simulation/torus/voronoi/dense and sparse/with/tVoronoisd.m
@@ -9,7 +9,9 @@ neighbor = zeros(numseed,2*size);
 maxv = zeros(numseed,1);
 spr = 0;
 %%
+clear title xlabel ylabel
 figure
+grid on
 for i = 1:size
     for j = 1:size
         
@@ -43,19 +45,52 @@ for k = 1:10
     tmp = pos{k};
     hold on
     plot(tmp(:,1), tmp(:,2),'k*');
+        txt = num2str(k);
+    text(tmp(1,1)+ 1,tmp(1,2),txt);
 end
 
 xlim([0 50]);
 ylim([0 50]);
 
-xlabel('latitude index');
-ylabel('longitude index');
-
+xlabel('Latitude index');
+ylabel('Longitude index');
+title('Toroidal Constrained Voronoi Cell');
 hold off
 
 %%
 figure
+S = [];
 hold on
+grid on
+[v,itmp] = min(maxv);
+neitmp = size*size;
+
+for i = 1:numseed
+    if maxv(i) == v
+        tmpneitmp = sum(neighbor(i,:));
+        if tmpneitmp < neitmp
+            neitmp = tmpneitmp;
+        end
+    end
+end
+
+for i = 1:numseed
+    sumtmp = 0;
+    for j = 1:2*size
+        sumtmp = sumtmp + neighbor(i,j);
+        if sumtmp >= neitmp
+            S = [S;j];
+            break;
+        else
+            if neighbor(i,j) == 0
+                S = [S;j];
+                break;
+            end
+        end
+    end
+end
+
+
 for i = 1:size
     for j = 1:size
         
@@ -71,7 +106,7 @@ for i = 1:size
         
         [minv,index] = min(sd);
         
-        if minv <= min(maxv)
+        if minv <= S(index)
             plot(i,j,'o','MarkerEdgeColor',[mcolor(index,:)],'MarkerFaceColor',[mcolor(index,:)]);
         else
             spr = spr + 1;
@@ -85,14 +120,16 @@ for k = 1:10
     tmp = pos{k};
     hold on
     plot(tmp(:,1), tmp(:,2),'k*');
+    txt = num2str(k);
+    text(tmp(1,1)+ 1,tmp(1,2),txt);
 end
 
 xlim([0 50]);
 ylim([0 50]);
 
-xlabel('latitude index');
-ylabel('longitude index');
-
+xlabel('Latitude index');
+ylabel('Longitude index');
+title('Toroidal Reduced Constrianed Voronoi Diagram');
 hold off
 
 %%
@@ -104,11 +141,10 @@ for i = 1:numseed
     title = [];
     title = [1 neighbor(i,1:maxv(i,1))];
     %title = [1 neighbor(i,1:min(maxv))];
-    sum(title)
-    sumcore = sumcore + sum(title);
-    nunitm(title);
+    nunitm_c(title,mcolor(i,:));
     %nunitm_no(title);
 end
+legend('1','2','3','4','5','6','7','8','9','10');
 hold off
 %%
 figure
@@ -118,11 +154,10 @@ sumcore = 0;
 for i = 1:numseed
     title = [];
     %title = [1 neighbor(i,1:maxv(i,1))];
-    title = [1 neighbor(i,1:min(maxv))];
-    sum(title)
-    sumcore = sumcore + sum(title);
-    nunitm(title);
+    title = [1 neighbor(i,1:S(i))];
+    nunitm_c(title,mcolor(i,:));
     %nunitm_no(title);
 end
+legend('1','2','3','4','5','6','7','8','9','10');
 hold off
 %%
